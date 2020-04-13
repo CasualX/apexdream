086ddb107c6f2bc2f6a891ef428b5e5002b584ec
Casper <CasualX@users.noreply.github.com>
2020-04-13 17:01:33 +0200

    Apex Legends: The Old Ways
    Combines a bunch of patches after the main update

--- src/data.hpp
+++ src/data.hpp
@@ -6,25 +6,25 @@ class GameData {
 public:
 	GameData() = default;
 
-	uint32_t time_date_stamp = 0x5e57306a;
-	uint32_t checksum = 0x1f741aa;
+	uint32_t time_date_stamp = 0x5e97b728;
+	uint32_t checksum = 0x1f873a4;
 
-	uint32_t client_state = 0x1137b80;
+	uint32_t client_state = 0x114bed0;
 	uint32_t client_signon_state = 0x0098;
 	uint32_t client_level_name = 0x01b8;
 
-	uint32_t view_render = 0xcb011e0;
+	uint32_t view_render = 0xcb0da70;
 	uint32_t view_matrix = 0x1b3bd0;
-	uint32_t global_vars = 0x1137880;
+	uint32_t global_vars = 0x114bbd0;
 
-	uint32_t input_system = 0x011a7600;
+	uint32_t input_system = 0x011bb900;
 	uint32_t input_button_state = 0xb0;
 
-	uint32_t in_attack = 0x0cb229f0;
-	uint32_t in_jump = 0x0cb22a60;
+	uint32_t in_attack = 0x0cb2f260;
+	uint32_t in_jump = 0x0cb2f2f0;
 
-	uint32_t local_entity = 0x1060b44;
-	uint32_t entity_list = 0x1883c38;
+	uint32_t local_entity = 0x1074c14;
+	uint32_t entity_list = 0x1897f38;
 
 	uint32_t entity_model_name = 0x0030;
 	uint32_t entity_flags = 0x0098;
@@ -46,8 +46,8 @@ public:
 	uint32_t player_zoom_state = 0x1ac0;
 	uint32_t player_camera_data = 0x1da8;
 	uint32_t player_bleedout_state = 0x2590;
-	uint32_t player_observer_mode = 0x31d4;
-	uint32_t player_helmet_armor_type = 0x410c;
+	uint32_t player_observer_mode = 0x320c;
+	uint32_t player_helmet_armor_type = 0x41a4;
 
 	uint32_t weaponx_weapon_owner = 0x1540;
 	uint32_t weaponx_player_data_zoom_fov = 0x165c;
@@ -56,7 +56,7 @@ public:
 
 	uint32_t prop_survival = 0x1544;
 
-	uint32_t player_resources = 0xcb22030;
+	uint32_t player_resources = 0xcb2e8c0;
 	uint32_t player_resource_names = 0x0a00;
 
 	uint32_t world_death_field = 0x0a40;
